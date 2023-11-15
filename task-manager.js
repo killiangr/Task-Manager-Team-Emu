4fe9247c7b924fc78e70706989c76bb32df324be
killiangr <killian.graindorge@efrei.net>
2023-11-15 11:54:30 +0100

    Test UpdateTaskList

--- task-manager.js
+++ task-manager.js
@@ -1,7 +1,27 @@
 function parseInput(input) { // + Learn PYthon
     const command = input.charAt(0);
     const id = parseInt(input.slice(1), 10);
-    return { command, id }; 
+    return { command, id };    
+   
 }
+
+function updateTaskList(tasks, command, id) {
+    switch (command) {
+      case '+':
+        tasks.push({ id: tasks.length + 1, description: id, status: 'to do' });
+        break;
+      case '-':
+        tasks = tasks.filter(task => task.id !== id);
+        break;
+      case 'x':
+        tasks.find(task => task.id === id).status = 'done';
+        break;
+      case 'o':
+        tasks.find(task => task.id === id).status = 'to do';
+        break;
+    }
+    return tasks;
+  }
   
- module.exports = {parseInput};
+  
+ module.exports = {parseInput,updateTaskList};
--- task-manager.test.js
+++ task-manager.test.js
@@ -1,8 +1,17 @@
-const { parseInput } = require('./task-manager');
+const { parseInput, updateTaskList } = require('./task-manager');
 
 test('parseInput should return command and id', () => {
     expect(parseInput('+ Learn Python')).toEqual({ command: '+', id: NaN });
-    //expect(parseInput('- 1')).toEqual({ command: '-', id: 1 });
-    });
+    expect(parseInput('- 1')).toEqual({ command: '-', id: 1 });
+});
+
+test('updateTaskList should update tasks based on command', () => {
+    const tasks = [];
+    
+    expect(updateTaskList(tasks, '+', 'Learn Python')).toHaveLength(1);
+    
+    const updatedTasks = updateTaskList(tasks, '+', 'Learn Python');
+    expect(updateTaskList(updatedTasks, 'x', 1)[0].status).toEqual('done');
+  });
 
   
