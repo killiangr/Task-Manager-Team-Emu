40a6f44877a15c95107649a9332146dcefd593e1
killiangr <killian.graindorge@efrei.net>
2023-11-15 12:28:36 +0100

    Final commit

--- task-manager.js
+++ task-manager.js
@@ -1,14 +1,16 @@
-function parseInput(input) { // + Learn PYthon
-    const command = input.charAt(0);
-    const id = parseInt(input.slice(1), 10);
-    return { command, id };    
+function parseInput(input) { // + Learn Python
+    const parts = input.split(' '); // array
+    const action = parts[0].toLowerCase(); //command
+    const id = parseInt(parts[1]);
+    const description = input.slice(1)
+    return { action, description, id };    
    
 }
 
-function updateTaskList(tasks, command, id) {
+function updateTaskList(tasks, command, id, description) {
     switch (command) {
       case '+':
-        tasks.push({ id: tasks.length + 1, description: id, status: 'to do' });
+        tasks.push({ id: tasks.length + 1, description: description, status: 'to do' });
         break;
       case '-':
         tasks = tasks.filter(task => task.id !== id);
@@ -23,5 +25,36 @@ function updateTaskList(tasks, command, id) {
     return tasks;
   }
   
+function displayTasks(tasks) {
+    tasks.forEach(task => {
+      const status = task.status === 'done' ? '[x]' : '[ ]';
+      console.log(`${task.id} ${status} ${task.description}`);
+    });
+  }
+
+const readline = require('readline');
+
+function runInteractionLoop() {
+  const tasks = [];
+  console.log("Task Manager - Enter commands(+ description, - id, x id, o id, q) : ");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.on('line', (input) => {
+    const { action, description, id } = parseInput(input);
+    const updateTasks = updateTaskList(tasks, action, id, description);
+    displayTasks(updateTasks);
+    
+    if (action === 'q') {
+        console.log("Exiting the interaction loop");
+        console.log('Bye!');
+        rl.close();
+    }
+  });
+}
+
+runInteractionLoop();
   
- module.exports = {parseInput,updateTaskList};
+ module.exports = {parseInput,updateTaskList,displayTasks, runInteractionLoop};
--- task-manager.test.js
+++ task-manager.test.js
@@ -1,17 +1,21 @@
-const { parseInput, updateTaskList } = require('./task-manager');
+const { parseInput, updateTaskList, displayTasks } = require('./task-manager');
 
-test('parseInput should return command and id', () => {
-    expect(parseInput('+ Learn Python')).toEqual({ command: '+', id: NaN });
-    expect(parseInput('- 1')).toEqual({ command: '-', id: 1 });
+test('parseInput should return action, description, id', () => {
+    expect(parseInput('+ Learn Python')).toEqual({ action: '+', description: " Learn Python", id: NaN });
+    //expect(parseInput('- 1')).toEqual({ action: '-', description: " 1", id: 1 });
 });
 
 test('updateTaskList should update tasks based on command', () => {
     const tasks = [];
-    
     expect(updateTaskList(tasks, '+', 'Learn Python')).toHaveLength(1);
-    
-    const updatedTasks = updateTaskList(tasks, '+', 'Learn Python');
     expect(updateTaskList(updatedTasks, 'x', 1)[0].status).toEqual('done');
   });
 
-  
+test('ShowTasks',() => {
+    const command = '+ Learn Python';
+    const parseInput = parseInput(command);
+    const task = [];
+    const updatedTasks = updateTaskList(task, parseInput.command, parseInput.id);
+    const printTasks = "1 [ ] Learn Python";
+    expect(displayTasks(updatedTasks).toEqual(printTasks));
+})
